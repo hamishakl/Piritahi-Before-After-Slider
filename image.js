50f4c31686d66f4fcb62f5a2fc378f50b3648369
Hamish Henare <hamishjustin7@gmail.com>
2022-02-18 12:10:17 +1300

    new functions for webflow to change images added

--- image.js
+++ image.js
@@ -1,20 +1,42 @@
-const owairakaBtn = document.getElementById('owairaka-btn')
-const roskillSouthBtn = document.getElementById('roskillsouth-btn')
-const waikowhaiBtn = document.getElementById('waikowhai-btn')
+const beforeImg = document.getElementById("before-img");
+const afterImg = document.getElementById("after-img");
 
-const aorereBtn = document.getElementById('aorere-btn')
-const mangereWestBtn = document.getElementById('mangereWest-btn')
+const owairaka = document.getElementById("owairaka-btn");
+owairaka.addEventListener("click", function owairakaChange() {
+  beforeImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e3ab55fb4a7b9537f0_OwairakaNow.jpg";
+  afterImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e11542028a13a2cc16_OwairakaBefore.jpg";
+});
 
-const owairakaImgBefore = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e11542028a13a2cc16_OwairakaBefore.jpg'
-const roskillSouthImgBefore = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e1f89fdd78f9d8a4da_RoskillSouthBefore.jpg'
-const waikowhaiImgBefore = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e16516da25bfd3bd26_WaikowhaiBefore.jpg'
+const roskillsouth = document.getElementById("roskillsouth-btn");
+roskillsouth.addEventListener("click", function roskillChange() {
+  beforeImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e1f89fdd78f9d8a4da_RoskillSouthBefore.jpg";
+  afterImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e33e800e0640a3ec64_RoskillSouthNow.jpg";
+});
 
-const owairakaImgAfter = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e3ab55fb4a7b9537f0_OwairakaNow.jpg'
-const roskillSouthImgAfter = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e33e800e0640a3ec64_RoskillSouthNow.jpg'
-const waikowhaiImgAfter = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e350ccd956c173f619_WaikowhaiNow.jpg'
+const waikowhai = document.getElementById("waikowhai-btn");
+waikowhai.addEventListener("click", function waikowhaiChange() {
+  beforeImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e16516da25bfd3bd26_WaikowhaiBefore.jpg";
+  afterImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e350ccd956c173f619_WaikowhaiNow.jpg";
+});
 
-const aorereImgBefore = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e07ed5e114f1395b0d_AorereBefore.jpg'
-const mangereWestImgBefore = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e2b3fd2b516789059b_MangereWestBefore.jpg'
+const mangereWest = document.getElementById("mangereWest-btn");
+mangereWest.addEventListener("click", function mangereWestChange() {
+  beforeImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e2b3fd2b516789059b_MangereWestBefore.jpg";
+  afterImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e2b9602e5012eb078b_MangereWestNow.jpg";
+});
 
-const aorereImgAfter = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e2c724774bdf1aef2c_AorereNow.jpg'
-const mangereWestImgAfter = 'https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e2b9602e5012eb078b_MangereWestNow.jpg'
+const aorere = document.getElementById("aorere-btn");
+aorere.addEventListener("click", function aorereChange() {
+  beforeImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e07ed5e114f1395b0d_AorereBefore.jpg";
+  afterImg.srcset =
+    "https://uploads-ssl.webflow.com/6088a238f6b62c4d3bae2127/61b9c1e2c724774bdf1aef2c_AorereNow.jpg";
+});
---
+++ nocomment.js
@@ -0,0 +1,276 @@
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+var supportsPassive = false;
+try {
+  window.addEventListener(
+    "test",
+    null,
+    Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      },
+    })
+  );
+} catch (e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent =
+  "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+
+function disableScroll() {
+  window.addEventListener("DOMMouseScroll", preventDefault, false);
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.addEventListener("touchmove", preventDefault, wheelOpt);
+  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+  window.removeEventListener("DOMMouseScroll", preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener("touchmove", preventDefault, wheelOpt);
+  window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+const mouseArea = document.getElementById("slider-crop");
+
+mouseArea.addEventListener("mouseenter", (e) => {
+  disableScroll();
+});
+
+mouseArea.addEventListener("mouseleave", (e) => {
+  enableScroll();
+});
+const imageContainer = document.querySelector(".slider--container");
+
+let minScale = 1;
+let maxScale = 4;
+let imageWidth;
+let imageHeight;
+let containerWidth;
+let containerHeight;
+let displayImageX = 0;
+let displayImageY = 0;
+let displayImageScale = 1;
+
+let displayDefaultWidth;
+let displayDefaultHeight;
+
+let rangeX = 0;
+let rangeMaxX = 0;
+let rangeMinX = 0;
+
+let rangeY = 0;
+let rangeMaxY = 0;
+let rangeMinY = 0;
+
+let displayImageRangeY = 0;
+
+let displayImageCurrentX = 0;
+let displayImageCurrentY = 0;
+let displayImageCurrentScale = 1;
+
+const slider = document.getElementById("slider--range");
+
+const sliderButton = document.getElementById("slider--button");
+
+const change = () => {
+  const fore = document.getElementById("after-img");
+  const border = document.getElementById("after-img-border");
+  let slideValue = document.getElementById("slider--range").value;
+  let slideValue2 = document.getElementById("slider--range").value * 1.002;
+  fore.style.clipPath =
+    "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+  border.style.clipPath =
+    "polygon(0 0," + slideValue2 + "% 0," + slideValue2 + "% 100%, 0 100%)";
+};
+
+function resizeContainer() {
+  containerWidth = imageContainer.offsetWidth;
+  containerHeight = imageContainer.offsetHeight;
+  if (displayDefaultWidth !== undefined && displayDefaultHeight !== undefined) {
+    displayDefaultWidth = displayImage.offsetWidth;
+    displayDefaultHeight = displayImage.offsetHeight;
+    updateRange();
+    displayImageCurrentX = clamp(displayImageX, rangeMinX, rangeMaxX);
+    displayImageCurrentY = clamp(displayImageY, rangeMinY, rangeMaxY);
+    updateDisplayImage(
+      displayImageCurrentX,
+      displayImageCurrentY,
+      displayImageCurrentScale
+    );
+  }
+}
+
+resizeContainer();
+
+function clamp(value, min, max) {
+  return Math.min(Math.max(min, value), max);
+}
+
+function clampScale(newScale) {
+  return clamp(newScale, minScale, maxScale);
+}
+
+window.addEventListener("resize", resizeContainer, true);
+
+const displayImage = document.getElementById("img--div");
+const preventDrag = () => {
+  imageWidth = displayImage.width;
+  imageHeight = displayImage.height;
+  displayImage.addEventListener("mousedown", (e) => e.preventDefault(), false);
+  displayDefaultWidth = displayImage.offsetWidth;
+  displayDefaultHeight = displayImage.offsetHeight;
+  rangeX = Math.max(0, displayDefaultWidth - containerWidth);
+  rangeY = Math.max(0, displayDefaultHeight - containerHeight);
+};
+preventDrag();
+imageContainer.addEventListener(
+  "wheel",
+  (e) => {
+    displayImageScale = displayImageCurrentScale = clampScale(
+      displayImageScale + e.wheelDelta / 800
+    );
+    updateRange();
+    displayImageCurrentX = clamp(displayImageCurrentX, rangeMinX, rangeMaxX);
+    displayImageCurrentY = clamp(displayImageCurrentY, rangeMinY, rangeMaxY);
+    updateDisplayImage(
+      displayImageCurrentX,
+      displayImageCurrentY,
+      displayImageScale
+    );
+  },
+  false
+);
+
+function updateDisplayImage(x, y, scale) {
+  const transform =
+    "translateX(" +
+    x +
+    "px) translateY(" +
+    y +
+    "px) translateZ(0px) scale(" +
+    scale +
+    "," +
+    scale +
+    ")";
+  displayImage.style.transform = transform;
+  displayImage.style.WebkitTransform = transform;
+  displayImage.style.msTransform = transform;
+}
+
+function updateRange() {
+  rangeX = Math.max(
+    0,
+    Math.round(displayDefaultWidth * displayImageCurrentScale) - containerWidth
+  );
+  rangeY = Math.max(
+    0,
+    Math.round(displayDefaultHeight * displayImageCurrentScale) -
+      containerHeight
+  );
+
+  rangeMaxX = Math.round(rangeX / 2);
+  rangeMinX = 0 - rangeMaxX;
+
+  rangeMaxY = Math.round(rangeY / 2);
+  rangeMinY = 0 - rangeMaxY;
+}
+
+const hammertime = new Hammer(imageContainer);
+const swiper = new Hammer(imageContainer);
+const emulator = new Hammer(imageContainer);
+hammertime.get("pinch").set({ enable: true, pointers: 2 });
+hammertime.get("pan").set({ direction: Hammer.DIRECTION_ALL, pointers: 2 });
+hammertime.get("rotate").set({ enable: true });
+
+hammertime.on("rotate", function (ev) {
+  console.log();
+});
+
+swiper.get("pan").set({ direction: Hammer.DIRECTION_ALL });
+
+swiper.on(" pan panleft panright panup pandown", function (ev) {
+  if (ev.srcEvent.shiftKey == false) {
+    if (ev.type === "panleft") {
+      change(slider.value--);
+    } else if (ev.type === "panright") {
+      change(slider.value++);
+    }
+  } else if (ev.srcEvent.shiftKey == true) {
+    moveOnPan(ev);
+  }
+});
+
+const moveOnPan = (ev) => {
+  displayImageCurrentX = clamp(displayImageX + ev.deltaX, rangeMinX, rangeMaxX);
+  displayImageCurrentY = clamp(displayImageY + ev.deltaY, rangeMinY, rangeMaxY);
+  updateDisplayImage(
+    displayImageCurrentX,
+    displayImageCurrentY,
+    displayImageScale
+  );
+};
+
+hammertime.on("pan", (ev) => {
+  console.log("pan");
+  displayImageCurrentX = clamp(displayImageX + ev.deltaX, rangeMinX, rangeMaxX);
+  displayImageCurrentY = clamp(displayImageY + ev.deltaY, rangeMinY, rangeMaxY);
+  updateDisplayImage(
+    displayImageCurrentX,
+    displayImageCurrentY,
+    displayImageScale
+  );
+});
+
+const moveOnPinch = (ev) => {
+  displayImageCurrentScale = clampScale(ev.scale * displayImageScale);
+  updateRange();
+  displayImageCurrentX = clamp(displayImageX + ev.deltaX, rangeMinX, rangeMaxX);
+  displayImageCurrentY = clamp(displayImageY + ev.deltaY, rangeMinY, rangeMaxY);
+  updateDisplayImage(
+    displayImageCurrentX,
+    displayImageCurrentY,
+    displayImageCurrentScale
+  );
+};
+
+hammertime.on("pinch pinchmove drag", (ev) => {
+  if (ev.pointerType === "touch") {
+    moveOnPinch(ev);
+    console.log("pinch");
+  } else {
+    moveOnPinch(ev);
+  }
+});
+
+hammertime.on("panend pancancel pinchend pinchcancel", () => {
+  displayImageScale = displayImageCurrentScale;
+  displayImageX = displayImageCurrentX;
+  displayImageY = displayImageCurrentY;
+});
+
+var elements = document.getElementsByClassName("div-block-19");
+var image = elements[0];
+
+const imgHeight = () => {
+  let height = document.getElementById("before-img").height;
+  image.style.height = height;
+  console.log(height);
+};
+
+imgHeight();
+
+window.onload = function () {
+  imgHeight();
+};
